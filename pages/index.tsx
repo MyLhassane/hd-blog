fdc315143d2291f3b4cde737d4e1cd0ecea29168
My Lhassane <mastinoz_7@hotmail.com>
2022-08-17 20:43:08 +0100

    Created the index setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,34 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { PostCard, Categories, PostWidget } from '../components';
 
 const posts = [
-  { title: 'React Testitng', excerpt: 'Learn React Testing'},
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'}
+  { title: "React Testitng", excerpt: "Learn React Testing" },
+  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
 ];
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {
-          posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))
-        }
+        {/* Posts */}
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+        </div>
+        {/* Two Gategories */}
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
