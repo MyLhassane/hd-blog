cbc28cdd8a6aecdbc88921acafc452e2483edb64
My Lhassane <mastinoz_7@hotmail.com>
2022-08-18 18:52:34 +0100

    Fetch Real Data from Graph CMS Using GraphQL

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import type { NextPage } from "next";
+// import type { NextPage } from "next";
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: "React Testitng", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
-
-const Home: NextPage = () => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,9 +13,9 @@ const Home: NextPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         {/* Posts */}
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
-        {/* Two Gategories */}
+        {/* Gategories */}
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
@@ -31,4 +27,11 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
+}
+
